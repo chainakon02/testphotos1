f35707e51d5d7805f0d68d393dde982e5ae35974
chainakon02 <chainakonsarisee@gmail.com>
2024-11-26 17:48:38 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const main = document.querySelector("main");
 
-document.querySelector(".scroller").addEventListener("scrollsnapchange", (event) => {
-  main.dataset.activeIndex = Math.round(event.target.scrollLeft / main.getBoundingClientRect().width);
+document.querySelector(".scroller").addEventListener("scroll", (event) => {
+  const scrollLeft = event.target.scrollLeft;
+  const itemWidth = main.getBoundingClientRect().width;
+
+  // คำนวณ active index
+  main.dataset.activeIndex = Math.round(scrollLeft / itemWidth);
 });
